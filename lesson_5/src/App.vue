b58b13a46312458f23030419605550b79322b1d0
Kenta Takeuchi <tkuc.kenta@gmail.com>
2020-09-12 20:55:11 +0900

    transition

---
+++ lesson_5/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="main">
+        <button @click="show = !show">切替</button>
+        <transition name="fade">
+            <p v-if="show">hello</p>
+        </transition>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        data() {
+            return {
+                show: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .fade-enter {
+        opacity: 0;
+    }
+
+    .fade-enter-active {
+        transition: opacity 5s;
+    }
+
+    .fade-enter-to {
+        opacity: 1;
+    }
+
+    .fade-leave {
+        opacity: 1;
+    }
+
+    .fade-leave-active {
+        transition: opacity 5s;
+    }
+
+    .fade-leave-to {
+        opacity: 0;
+    }
+
+    .main {
+        width: 70%;
+        margin: auto;
+        padding-top: 5rem;
+        text-align: center;
+    }
+
+
+</style>
